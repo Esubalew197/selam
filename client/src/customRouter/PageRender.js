0de0bf70199ec2fb2933eec95c31e0b9736f8219
Esubalew197 <Esubalewmillion@gmail.com>
2024-03-24 17:01:03 +0300

    Update files

--- client/src/customRouter/PageRender.js
+++ client/src/customRouter/PageRender.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useParams} from 'react-router-dom'
 import NotFound from '../components/NotFound'
+import { useSelector } from 'react-redux'
 
 const generatePage = (pageName) => {
     const component = () => require(`../pages/${pageName}`).default
@@ -12,20 +13,21 @@ const generatePage = (pageName) => {
     }
 }
 
-
 const PageRender = () => {
     const {page, id} = useParams()
-    //const { auth } = useSelector(state => state)
+    const { auth } = useSelector(state => state)
 
-    let pageName = "";    
+    let pageName = "";
+
+    if(auth.token){
         if(id){
             pageName = `${page}/[id]`
         }else{
             pageName = `${page}`
         }
-        console.log(pageName)
-        return generatePage(pageName)
+    }
 
+    return generatePage(pageName)
 }
 
 export default PageRender
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const dotenv = require("dotenv");
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const SocketServer = require('./socketServer')
+const { ExpressPeerServer } = require('peer')
+const path = require('path')
 
 
 const app = express()
@@ -11,30 +14,47 @@ app.use(cors())
 app.use(cookieParser())
 
 
+// Socket
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+io.on('connection', socket => {
+    SocketServer(socket)
+})
+
+// Create peer server
+ExpressPeerServer(http, { path: '/' })
+
+
 // Routes
 app.use('/api', require('./routes/authRouter'))
+app.use('/api', require('./routes/userRouter'))
+app.use('/api', require('./routes/postRouter'))
+app.use('/api', require('./routes/commentRouter'))
+app.use('/api', require('./routes/notifyRouter'))
+app.use('/api', require('./routes/messageRouter'))
 
 
-dotenv.config();
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => {
+    if(err) throw err;
+    console.log('Connected to mongodb')
+})
 
-async function connectToDatabase() {
-    try {
-      await mongoose.connect(process.env.MONGODB_URL, {
-        //useCreateIndex: true,
-        //useFindAndModify: false,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log("Connected to MongoDB");
-    } catch (error) {
-      console.error("Error connecting to MongoDB:", error);
-    }
-  }
-  
-  connectToDatabase();
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 
 const port = process.env.PORT || 5000
-app.listen(port, () => {
+http.listen(port, () => {
     console.log('Server is running on port', port)
 })
